c5c71e4465d34a47e7bd84d9dda4f036aa178327
sbkelemen <sara.kelemen@bca.hu>
2024-05-17 18:07:12 +0200

    Contact age view example added

--- DevClassic/SqlScripts/UsrCreateContactAgeDaysView/UsrCreateContactAgeDaysView.sql
+++ DevClassic/SqlScripts/UsrCreateContactAgeDaysView/UsrCreateContactAgeDaysView.sql
@@ -1,5 +1,6 @@
 create or alter view UsrVwContactAgeDays
 as
 select Id as UsrId, Name as UsrName, BirthDate as UsrBirthDate,
-datediff(day, BirthDate, getdate()) as UsrAgeDays
+datediff(day, BirthDate, getdate()) as UsrAgeDays,
+Id as UsrContactid
 from Contact
